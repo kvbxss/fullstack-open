8e944f3cefb370d97d84a7d9941fad21b26bfc03
kvbxss <kubadownar444@gmail.com>
2023-07-03 20:33:06 +0200

    halfway

---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+
+const Filter = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+  return (
+    <div>Filter</div>
+  )
+}
+
+export default Filter
