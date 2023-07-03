38b34a227749dd578dd886f53be611374a98863a
kvbxss <kubadownar444@gmail.com>
2023-07-03 17:39:44 +0200

    part2 course info step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+const Header = ({ courseName }) => {
+  return (
+    <h1>{courseName}</h1>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} - {part.exercises} exercises
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+
+  return (
+    <>
+      <Course course={course} />
+    </>
+     
+  )
+}
+
+export default App
