b1dc5741dc180e09cc17ccf6c62baacd921e8c16
kvbxss <kubadownar444@gmail.com>
2023-07-03 19:04:43 +0200

    2.7 phonebook step2

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (newName.trim() === '') {
+      return; // Ignore empty names
+    }
+
+    const isDuplicate = persons.some((person) => person.name.toLowerCase() === newName.toLowerCase());
+
+    if (isDuplicate) {
+      alert(`${newName} is already added to the phonebook!`);
+      return;
+    }
+    
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons([...persons, newPerson]);
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
