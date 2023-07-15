5b1612cbd0dc96922c6464094349fd09f8a37ad1
kvbxss <kubadownar444@gmail.com>
2023-07-15 16:35:34 +0200

    step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import SearchFilter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Person from './components/Persons';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import SearchFilter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Person from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
-
-
-  
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -28,7 +25,7 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
 
-    if (newName.trim() === '' || newNumber.trim() === '') {
+    if (newName.trim() === "" || newNumber.trim() === "") {
       return; // Ignore empty name or number
     }
 
@@ -46,33 +43,49 @@ const App = () => {
       number: newNumber,
     };
 
+    axios.post("http://localhost:3001/persons", newPerson)
+    .then((response) => {
+      setPersons([...persons, response.data]);
+      setNewName("");
+      setNewNumber("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    
     setPersons([...persons, newPerson]);
-    setNewName('');
-    setNewNumber('');
+    setNewName("");
+    setNewNumber("");
   };
+
+  
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-
   const hook = () => {
-    console.log('effect');
-    axios
-    .get('http://localhost:3001/persons')
-    .then((response) => {
-      console.log('promise fulfilled');
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
       setPersons(response.data);
     });
-  }
+  };
+
+ 
+  
 
   useEffect(hook, []);
-  console.log('render', persons.length, 'persons')
-  
+  console.log("render", persons.length, "persons");
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <SearchFilter searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
+      <SearchFilter
+        searchQuery={searchQuery}
+        handleSearchChange={handleSearchChange}
+      />
       <h3>Add a new person</h3>
       <PersonForm
         newName={newName}
