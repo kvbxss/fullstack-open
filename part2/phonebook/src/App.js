df70cd4c6e31faa7eef3fe2b82518eeec64540f8
kvbxss <kubadownar444@gmail.com>
2023-07-16 14:00:59 +0200

    step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,14 @@ import PersonForm from "./components/PersonForm";
 import Person from "./components/Persons";
 
 import personService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const [notification, setNotification] = useState(null);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -27,7 +29,12 @@ const App = () => {
     setPersons(persons.filter((person) => person.id !== id));
   };
 
- 
+  const showNotification = (message) => {
+    setNotification(message);
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000); // Duration in milliseconds (3 seconds)
+  };
     
 
   const addPerson = (event) => {
@@ -59,6 +66,7 @@ const App = () => {
             );
             setNewName("");
             setNewNumber("");
+            showNotification(`Updated ${response.name}`);
           })
           .catch((error) => {
             console.log(error);
@@ -78,6 +86,7 @@ const App = () => {
         setPersons([...persons, response]);
         setNewName("");
         setNewNumber("");
+        showNotification(`Added ${response.name}`);
       })
       .catch((error) => {
         console.log(error);
@@ -118,6 +127,7 @@ const App = () => {
         searchQuery={searchQuery}
         handleSearchChange={handleSearchChange}
       />
+       {notification && <Notification message={notification} />}
       <h3>Add a new person</h3>
       <PersonForm
         newName={newName}
