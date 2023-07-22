04f723352d49679492333d60b01aad5ac846ddd6
kvbxss <kubadownar444@gmail.com>
2023-07-22 18:43:18 +0200

    step3.9

--- part2/phonebook/src/services/notes.js
+++ part2/phonebook/src/services/notes.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
-    return response.data;
+  return response.data;
 };
 
 const create = async (newPerson) => {
   const response = await axios.post(baseUrl, newPerson);
-    return response.data;
+  return response.data;
 };
 const remove = async (id) => {
-    const response = await axios.delete(`${baseUrl}/${id}`);
-    return response.data;
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  return response.data;
 };
 const update = async (id, newPerson) => {
-    const response = await axios.put(`${baseUrl}/${id}`, newPerson);
-    return response.data;
+  const response = await axios.put(`${baseUrl}/${id}`, newPerson);
+  return response.data;
 };
 
 const personService = {
